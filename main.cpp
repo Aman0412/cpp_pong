470341f31140cbe6f4a184d95692c18ce2f50482
[Aman Praveen] <[AmanPraveen70@gmail.com]>
2021-01-24 13:16:19 +0000

    now added two score counters, a middle line and a ball

--- main.cpp
+++ main.cpp
@@ -15,6 +15,35 @@ int main()
 	sf::RectangleShape p2(sf::Vector2f(25.0f,75.0f));
 	p2.setPosition(740.f,163.5f);
 
+	//create score count
+	sf::Text p1score;
+	sf::Font p1text;
+	if (!p1text.loadFromFile("C:\\Users\\amanp\\Downloads\\fff-forward\\FFFFORWA.ttf"))
+		{
+			std::cout<<"cannot load font" << std::endl;
+		}
+	p1score.setFont(p1text);
+	p1score.setString("0");
+	p1score.setPosition(325.f,20.0f);
+	p1score.setCharacterSize(50);
+	
+	sf::Text p2score;
+	p2score.setFont(p1text);
+	p2score.setString("0");
+	p2score.setCharacterSize(50);
+	p2score.setPosition(450.f,20.0f);
+
+
+	//create ball
+	sf::CircleShape ball(12.5f);
+	ball.setPosition(400.f,200.f);
+	ball.setOrigin(ball.getRadius(),ball.getRadius());
+	
+	//create middle line 
+	sf::RectangleShape midline(sf::Vector2f(10.f,1000000.f));
+	midline.setOrigin(5.f,500000.f);
+	midline.setPosition(400.f,200.f);
+
 	while (window.isOpen())
 	{
 		//event is for if the the close button is pressed, it will close the game
@@ -89,11 +118,20 @@ int main()
 				p2.move(0.f,-0.5f);
 			}
 		}
+		
+		//move our ball
+//		ball.move(-.5f,-.5f);
+		
+
 
 		window.clear();
 		window.draw(p1);
+		window.draw(p1score);
+		window.draw(p2score);
 		window.draw(p2);
+		window.draw(ball);
+		window.draw(midline);
 		window.display();
-
+		
 	}
 }
